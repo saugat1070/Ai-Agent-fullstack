ab968151f3763ec27c1ffbc069f288e8a6d3a5bf
saugat1070 <saugatgiri1070@gmail.com>
2025-07-31 13:53:02 +0545

    starting fronted

---
+++ fronted-ai/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route
+      path='/'
+
+      />
+    </Routes>
+    
+    </BrowserRouter>
+  </StrictMode>,
+)
---
+++ fronted-ai/src/pages/singup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Singup() {
+  return (
+    <div>singup</div>
+  )
+}
+
+export default Singup
